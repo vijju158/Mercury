e905a1503d9ed68b395abdb176b2630419c08e6f
E000748 <E000748@CTLINDT-0231.cignitiglobal.com>
2019-09-05 13:53:14 +0530

    Test Commit

---
+++ Batch.bat
@@ -0,0 +1,3 @@
+cd C:\Users\E000748\eclipse-workspace\MercuryAutomation
+java -cp C:\Users\E000748\eclipse-workspace\MercuryAutomation\lib\*;C:\Users\E000748\eclipse-workspace\MercuryAutomation\target\test-classes org.testng.TestNG testng.xml
+pause
